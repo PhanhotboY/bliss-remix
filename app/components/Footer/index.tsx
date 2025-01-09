fc0050d33d5533bd147b97e84843b793272c42a7
Phan <nguyenduyphan2003@gmail.com>
2025-01-09 23:27:18 +0700

    fix/resize footer logo

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -28,10 +28,10 @@ export default function Footer() {
         <section className='col-span-12 md:col-span-4 flex flex-col gap-4'>
           <Heading3>VỀ CHÚNG TÔI</Heading3>
 
-          <div className='w-full col-span-4 md:items-center px-8'>
+          <div className='w-full aspect-video col-span-4 md:items-center px-8'>
             <Link to='/'>
               <img
-                className='h-full w-full object-contain object-left z-0'
+                className='object-contain object-center z-0'
                 src={app.app_logo}
                 alt={app.app_title}
               />
